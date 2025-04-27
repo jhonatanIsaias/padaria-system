9b7f60e2ffc5eed4f7aec1b16bfb8ae6ed3db324
jhonatan <jhonatan.santos13@academico.ifs.edu.br>
2025-04-26 21:34:48 -0300

    erro dto corrigido

--- src/modules/user/dtos/usuario-dto.ts
+++ src/modules/user/dtos/usuario-dto.ts
@@ -3,19 +3,14 @@ import { usuarios } from '@prisma/client';
 export class UsuarioDto {
   id:string;
   nome: string;
-  permissao: boolean;
-  login: string;
   email: string;
-  created_at:Date;
+
 
   static of(user: usuarios): UsuarioDto {
     const data = new UsuarioDto();
     data.id = user.id;
     data.nome = user.nome!;
     data.email = user.email!;
-    data.login = user.login!;
-    data.permissao = !!user.permissao; 
-    data.created_at = user.created_at;
     return data;
   }
 }
